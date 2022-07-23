e02a7b2fd7b5947af9ea59134c5d201ce08019fd
NguyenMinhHieuk22 <109660668+NguyenMinhHieuk22@users.noreply.github.com>
2022-07-23 16:19:28 +0700

    Update CarManager.java

--- CarManager.java
+++ CarManager.java
@@ -17,17 +17,17 @@ public class CarManager {
         Menu<String> menu = new Menu<String>();
 
         ArrayList<String> ops = new ArrayList<String>();
-        ops.add("1.List all brands.");
-        ops.add("2.Add a new brand.");
-        ops.add("3.Search a brand based on its ID.");
-        ops.add("4.Update a brand.");
-        ops.add("5.Save brands to the file, named brands.txt.");
-        ops.add("6.List all cars in ascending order of brand names.");
-        ops.add("7.List cars based on a part of an input brand name.");
-        ops.add("8.Add a car.");
-        ops.add("9.Remove a car based on its ID.");
-        ops.add("10.Update a car based on its ID.");
-        ops.add("11.Save cars to file, named cars.txt.");
+        ops.add("List all brands.");
+        ops.add("Add a new brand.");
+        ops.add("Search a brand based on its ID.");
+        ops.add("Update a brand.");
+        ops.add("Save brands to the file, named brands.txt.");
+        ops.add("List all cars in ascending order of brand names.");
+        ops.add("List cars based on a part of an input brand name.");
+        ops.add("Add a car.");
+        ops.add("Remove a car based on its ID.");
+        ops.add("Update a car based on its ID.");
+        ops.add("Save cars to file, named cars.txt.");
 
         do {
             choice = menu.int_getChoice(ops);
